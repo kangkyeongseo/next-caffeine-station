8366d9c676679e825296081122213a23b8a4c083
kangkyeongseo <kks_big@naver.com>
2024-01-18 23:22:05 +0900

    Feat: 키워드 검색 및 마커 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,30 @@ declare global {
 export default function Home() {
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<any>(null);
+  const [ps, setPs] = useState<any>(null);
+
+  const placesSearchCB = (data, status, pagination) => {
+    if (status === window.kakao.maps.services.Status.OK) {
+      const bounds = new window.kakao.maps.LatLngBounds();
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        displayMarker(data[i]);
+        bounds.extend(new window.kakao.maps.LatLng(data[i].y, data[i].x));
+      }
+
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+      map.setBounds(bounds);
+    } else {
+      console.log('검색 실패', window.kakao.maps.services.Status);
+    }
+  };
+
+  const displayMarker = place => {
+    const marker = new window.kakao.maps.Marker({
+      map,
+      position: new window.kakao.maps.LatLng(place.y, place.x),
+    });
+  };
 
   useEffect(() => {
     window.kakao.maps.load(() => {
@@ -20,8 +44,19 @@ export default function Home() {
       };
 
       setMap(new window.kakao.maps.Map(mapRef.current, options));
+      setPs(new window.kakao.maps.services.Places());
     });
   }, []);
+
+  useEffect(() => {
+    if (!map) return;
+    if (!ps) return;
+    ps.setMap(map);
+    ps.keywordSearch('빽다방', placesSearchCB, {
+      useMapCenter: true,
+      radius: 1000,
+    });
+  }, [map, ps]);
 
   return <div ref={mapRef} className='h-96 w-96'></div>;
 }

7c32ce4ad98ccdebe1cfc5392bb294b4a6b48142
kangkyeongseo <kks_big@naver.com>
2024-01-18 23:20:20 +0900

    Feat: 카카오 지도 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 export default function Home() {
-  return <div>main</div>;
+  const mapRef = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<any>(null);
+
+  useEffect(() => {
+    window.kakao.maps.load(() => {
+      const options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new window.kakao.maps.LatLng(37.639888, 126.791622), //지도의 중심좌표.
+        level: 3, //지도의 레벨(확대, 축소 정도)
+      };
+
+      setMap(new window.kakao.maps.Map(mapRef.current, options));
+    });
+  }, []);
+
+  return <div ref={mapRef} className='h-96 w-96'></div>;
 }

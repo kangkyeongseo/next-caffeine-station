4f59ba4d9b0e49989b6cea506c14b276895bd302
kangkyeongseo <kks_big@naver.com>
2024-04-05 19:28:57 +0900

    Fix: 재검색 후 지도조작 시 잔상이 생기는 오류 해결

--- src/components/Main/Map.tsx
+++ src/components/Main/Map.tsx
@@ -35,19 +35,21 @@ const Map = () => {
 
   useEffect(() => {
     if (!coordsState) return;
-    window.kakao.maps.load(() => {
-      const options = {
-        //지도를 생성할 때 필요한 기본 옵션
-        center: new window.kakao.maps.LatLng(
-          coordsState.latitude,
-          coordsState.longitude,
-        ), //지도의 중심좌표.
-        level: 2, //지도의 레벨(확대, 축소 정도)
-      };
-      const newMap = new window.kakao.maps.Map(mapRef.current, options);
-      displayCurrentMarker(newMap, coordsState);
-      dispatch(setMap(newMap));
-    });
+    if (!map) {
+      window.kakao.maps.load(() => {
+        const options = {
+          //지도를 생성할 때 필요한 기본 옵션
+          center: new window.kakao.maps.LatLng(
+            coordsState.latitude,
+            coordsState.longitude,
+          ), //지도의 중심좌표.
+          level: 2, //지도의 레벨(확대, 축소 정도)
+        };
+        const newMap = new window.kakao.maps.Map(mapRef.current, options);
+        displayCurrentMarker(newMap, coordsState);
+        dispatch(setMap(newMap));
+      });
+    }
   }, [coordsState]);
 
   return (

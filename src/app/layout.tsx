a3fcefc6ab031acf9ae072b5201a6aa0990ebe52
kangkyeongseo <kks_big@naver.com>
2024-06-13 14:45:28 +0900

    Style: 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localfont from 'next/font/local';
 import Script from 'next/script';
 import './globals.css';
 import Provider from '@/components/Provider';
 import { rootMetadata } from './constants/metadatas';
 
-const inter = Inter({ subsets: ['latin'] });
+const pretendard = localfont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+});
 
 export const metadata: Metadata = rootMetadata;
 
@@ -18,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ko'>
-      <body className={inter.className}>
+      <body className={pretendard.className}>
         <Script
           strategy='beforeInteractive'
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`}

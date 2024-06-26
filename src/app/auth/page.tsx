0d1a349cd3741d9ead5aedc95ac375d5f9a63092
kangkyeongseo <kks_big@naver.com>
2024-06-26 14:37:36 +0900

    Style: Auth Page 모바일 반응형 스타일

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,7 +3,7 @@ import AuthContainer from '@/components/Auth/AuthContainer';
 
 function LoginPage() {
   return (
-    <div className='flex h-screen w-screen items-center justify-center bg-black/80'>
+    <div className='flex h-dvh w-screen items-center justify-center bg-black/80'>
       <AuthContainer />
     </div>
   );
--- src/components/Auth/Password/PasswordForm.tsx
+++ src/components/Auth/Password/PasswordForm.tsx
@@ -29,7 +29,7 @@ const PasswordForm = ({ type, setType }: AuthFormProps) => {
       <AuthHeader type={type} />
       <div className='mt-20 flex justify-center'>
         <form
-          className='w-full space-y-3 px-12'
+          className='w-full space-y-3 px-10 lg:px-12'
           onSubmit={handleSubmit(onLoginValid)}
         >
           <div className='relative'>
